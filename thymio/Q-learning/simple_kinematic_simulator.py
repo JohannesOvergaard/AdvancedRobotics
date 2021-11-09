62037a079caeacb66e7a9dcdc7d6a6968ab42558
JOVE <jove@itu.dk>
2021-11-09 08:34:01 +0100

    object avoidance

--- thymio/Q-learning/simple_kinematic_simulator.py
+++ thymio/Q-learning/simple_kinematic_simulator.py
@@ -12,8 +12,8 @@ from matplotlib.patches import Rectangle
 R = 0.021  # radius of wheels in meters
 L = 0.093  # distance between wheels in meters
 
-W = 1.92  # width of arena
-H = 1.14  # height of arena
+W = 1.92/2  # width of arena
+H = 1.14/2  # height of arena
 
 robot_timestep = 0.1        # 1/robot_timestep equals update frequency of robot
 simulation_timestep = 0.01  # timestep in kinematics sim (probably don't touch..)
@@ -33,15 +33,15 @@ left_wheel_velocity =  random()   # robot left wheel velocity in radians/s
 right_wheel_velocity =  random()  # robot right wheel velocity in radians/s
 
 #Q-learning
-action_space = np.array([(50,200),(200,50),(200,200)], dtype="i,i") #Turn left, turn right, forward
+action_space = np.array([(-1,1),(1,-1),(1,1)], dtype="i,i") #Turn left, turn right, forward
 state_space = np.array(["OL", "OR", "OF", "NO"]) #OL, OR, OF, NO
 
 Q = np.zeros((len(state_space),len(action_space)))
 print (Q)
 
-epsilon = 0.2
-decay = 0.99
-lr = 0.99
+epsilon = 1
+decay = 0.999
+lr = 0.95
 gamma = 0.9
 
 # Kinematic model
@@ -56,9 +56,13 @@ def simulationstep():
         v_y = sin(q)*(R*left_wheel_velocity/2 + R*right_wheel_velocity/2)
         omega = (R*right_wheel_velocity - R*left_wheel_velocity)/(2*L)    
     
-        x += v_x * simulation_timestep
-        y += v_y * simulation_timestep
+        xx = x + v_x * simulation_timestep
+        yy = y + v_y * simulation_timestep
         q += omega * simulation_timestep
+        
+        if (0<xx<W and 0<yy<H):
+            x = xx
+            y = yy
 
 def setup():
     ray_0 = LineString([(x, y), (x+cos(q+0.7)*(W+H),(y+sin(q+0.7)*(W+H))) ])
@@ -80,28 +84,29 @@ def setup():
     return sensor_to_state([s0_dist,s1_dist,s2_dist,s3_dist,s4_dist])
 
 def sensor_to_state(sensor_values):
-    if (sensor_values[0] < min_distance_wall):
-        return state_space[0]
-    if (sensor_values[4] < min_distance_wall):
-        return state_space[1]
-    if (sensor_values[1] < min_distance_wall or sensor_values[2] < min_distance_wall or sensor_values[3] < min_distance_wall):
-        return state_space[2]
+    min_dist = min(sensor_values)
+    if (min_dist < min_distance_wall):
+        sensor_index = sensor_values.index(min_dist)
+        if (sensor_index == 0 or sensor_index == 1):
+            return state_space[0]
+        if (sensor_index == 3 or sensor_index == 4):
+            return state_space[1]
+        if (sensor_index == 2):
+            return state_space[2]
     else:
         return state_space[3]
-
+        
 def explore_or_exploit(state):
     global epsilon
     epsilon = epsilon*decay
     if uniform(0,1) < epsilon:  #Explore
-       return np.random.choice(action_space)
+        return np.random.choice(action_space)
     else:                       #Exploit
-       return get_max_reward(state)
+        return get_max_action(state)
 
-def get_max_reward(state):
+def get_max_action(state):
     s = np.where(state_space == state)[0][0]
-    print (s)
     a = np.argmax(Q[s,:])
-    print(a)
     return action_space[a]
 
 def updateQ(state, action, reward, new_state):
@@ -114,7 +119,6 @@ def do_action(state, action):
     global left_wheel_velocity, right_wheel_velocity, x, y, q
 
     left_wheel_velocity, right_wheel_velocity = action
-    
     simulationstep()
 
     ray_0 = LineString([(x, y), (x+cos(q+0.7)*(W+H),(y+sin(q+0.7)*(W+H))) ])
@@ -134,19 +138,35 @@ def do_action(state, action):
     s4_dist = sqrt((s4.x-x)**2+(s4.y-y)**2)                    # distance to wall
 
     new_state = sensor_to_state([s0_dist,s1_dist,s2_dist,s3_dist,s4_dist])
-    print (new_state)
-    reward = get_reward(state)
-    print (reward)
+    reward = get_reward(state, action)
 
     updateQ(state,action,reward,new_state)
 
     return new_state
 
-def get_reward(state):
+def get_reward(state, action):
+    i = 0
+    a = np.where(action_space == action)
+    if (a[0][0] == 2):  #Motivation to drive forward
+        i = 5
     if (state == "NO"):
-        return 5
+        return i
     else:
         return -1
+    # i = 0
+    # a = np.where(action_space == action)
+    # if (a[0][0] == 2):
+    #     i = 10
+    # if ((state == "OL" or state == "OR" or state == "OF") and new_state == "NO"):
+    #     return 100+i
+    # elif (new_state == "NO"):
+    #     return i
+    # elif (state == new_state):
+    #     return -1
+    # elif (new_state == "OL" or new_state == "OR" or new_state == "OF"):
+    #     return -5
+    # else:
+    #     return 0
 
 
 # Simulation loop
@@ -163,22 +183,20 @@ ax.add_patch( Rectangle((0, 0),
                         lw = 3) )
 
 state = setup()
-print (state)
 
-for cnt in range(500):
+for cnt in range(10000):
     action = explore_or_exploit(state)
-    print (action)
 
     state = do_action(state, action)
 
     #check collision with arena walls 
-    if (world.distance(Point(x,y))<L/2):
-        break
+    # if (world.distance(Point(x,y))<L/2):
+    #     break
         
-    if cnt%5==0:
+    if cnt%50==0:
         #file.write( str(x) + ", " + str(y) + ", " + str(cos(q-0.7)*0.05) + ", " + str(sin(q-0.7)*0.05) + "\n")
         #file.write( str(x) + ", " + str(y) + ", " + str(cos(q-0.35)*0.05) + ", " + str(sin(q-0.35)*0.05) + "\n")
-        file.write( str(x) + ", " + str(y) + ", " + str(cos(q)*0.05) + ", " + str(sin(q)*0.05) + "\n")
+        #file.write( str(x) + ", " + str(y) + ", " + str(cos(q)*0.05) + ", " + str(sin(q)*0.05) + "\n")
         #file.write( str(x) + ", " + str(y) + ", " + str(cos(q+0.35)*0.05) + ", " + str(sin(q+0.35)*0.05) + "\n")
         #file.write( str(x) + ", " + str(y) + ", " + str(cos(q+0.7)*0.05) + ", " + str(sin(q+0.7)*0.05) + "\n")
 
@@ -193,7 +211,7 @@ for cnt in range(500):
         
         plt.draw() 
         plt.pause(0.01) #is necessary for the plot to update for some reason
-
+print(Q)
 file.close()
 plt.show(block=True)
     
